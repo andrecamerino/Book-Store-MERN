6feef1c54a7820b0813088c26bb75cc9146ab7a2
andrecamerino <camerinoandre@gmail.com>
2024-11-29 04:29:51 +1300

    first MERN commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://andrecamerino:firstmernstack@books-store-mern.gbfkb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
